b6944f3b61b5450436a7b9f8f23a50dbb14d7c8f
sid-FBLA <sid20102004@gmail.com>
2021-02-18 15:42:51 -0500

    Done -- basically

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-//Allows for modals to show
+window.addEventListener('DOMContentLoaded', (event) => {
 
-$('#myModal').on('shown.bs.modal', function () {
-  $('#myInput').trigger('focus')
-})
+  //Allows for modals to show
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
 
-//Enables scrollspy
-$('body').scrollspy({ target: '#navbar-example' })
+  //Enables scrollspy
+  $('body').scrollspy({ target: '#navbar-example' })
+
+  //Collapsible
+  $('#myCollapsible').collapse({
+    toggle: false
+  })
+
+  $('.collapse').collapse()
+
+  //Adding Offset
+  let offset = 180;
+
+  $('.navbar li a').click(function(event) {
+      event.preventDefault();
+      $($(this).attr('href'))[0].scrollIntoView();
+      scrollBy(0, -offset);
+  });
+
+}
